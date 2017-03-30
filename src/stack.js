23f7c874e1413fe32523e2e48fb935003fd1116f
George Crabtree <george.crabtree@bigcommerce.com>
2017-03-31 06:06:21 +1100

    Add basic stack implementation

---
+++ src/stack.js
@@ -0,0 +1,25 @@
+import {pushHead as push} from './core/methods';
+import {popHead as pop} from './core/methods';
+import {createNode} from './core/nodes';
+import {headToTail} from './core/iterators';
+
+function createStack() {
+
+    const stack = {
+        pop,
+        push,
+        createNode,
+        size: 0,
+        [Symbol.iterator]: headToTail
+    };
+
+    return {
+        pop: pop.bind(stack),
+        push: push.bind(stack),
+        get size() {
+            return stack.size;
+        },
+        [Symbol.iterator]: headToTail.bind(stack)
+    }
+}
+
